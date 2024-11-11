ba1f4e9e1c2d695d5361e7cfb246d6a06ad9b77f
Volodymyr_Voitovych <Volodymyr_Voitovych@epam.com>
2024-11-11 21:58:04 +0200

    minor changes

---
+++ DataStructures/Tasks/DoublyLinkedList.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tasks.DoNotChange;
+
+namespace Tasks
+{
+    public class DoublyLinkedList<T> : IDoublyLinkedList<T>
+    {
+        private class Node
+        {
+            public T Data;
+            public Node Next;
+            public Node Previous;
+
+            public Node(T data)
+            {
+                Data = data;
+                Next = null;
+                Previous = null;
+            }
+        }
+
+        private Node first;
+        private Node last;
+        private int count;
+
+        public int Length => count;
+
+        public void Add(T e)
+        {
+            Node newNode = new Node(e);
+            if (first == null)
+            {
+                first = newNode;
+                last = newNode;
+            }
+            else
+            {
+                last.Next = newNode;
+                newNode.Previous = last;
+                last = newNode;
+            }
+            count++;
+        }
+
+        public void AddAt(int index, T e)
+        {
+            if (index < 0 || index > count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            Node newNode = new Node(e);
+            if (index == 0)
+            {
+                if (first == null)
+                {
+                    first = newNode;
+                    last = newNode;
+                }
+                else
+                {
+                    newNode.Next = first;
+                    first.Previous = newNode;
+                    first = newNode;
+                }
+            }
+            else if (index == count)
+            {
+                last.Next = newNode;
+                newNode.Previous = last;
+                last = newNode;
+            }
+            else
+            {
+                Node current = first;
+                for (int i = 0; i < index; i++)
+                {
+                    current = current.Next;
+                }
+                newNode.Next = current;
+                newNode.Previous = current.Previous;
+                current.Previous.Next = newNode;
+                current.Previous = newNode;
+            }
+            count++;
+        }
+
+        public T ElementAt(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new IndexOutOfRangeException(nameof(index));
+
+            Node current = first;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+            return current.Data;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = first;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        public void Remove(T item)
+        {
+            Node current = first;
+            while (current != null)
+            {
+                if (current.Data.Equals(item))
+                {
+                    if (current.Previous != null)
+                    {
+                        current.Previous.Next = current.Next;
+                    }
+                    else
+                    {
+                        first = current.Next;
+                    }
+
+                    if (current.Next != null)
+                    {
+                        current.Next.Previous = current.Previous;
+                    }
+                    else
+                    {
+                        last = current.Previous;
+                    }
+
+                    count--;
+                    return;
+                }
+                current = current.Next;
+            }
+        }
+
+        public T RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new IndexOutOfRangeException(nameof(index));
+
+            Node current = first;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+
+            if (current.Previous != null)
+            {
+                current.Previous.Next = current.Next;
+            }
+            else
+            {
+                first = current.Next;
+            }
+
+            if (current.Next != null)
+            {
+                current.Next.Previous = current.Previous;
+            }
+            else
+            {
+                last = current.Previous;
+            }
+
+            count--;
+            return current.Data;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ DataStructures/Tasks/HybridFlowProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Tasks.DoNotChange;
+
+namespace Tasks
+{
+    public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
+    {
+        private readonly DoublyLinkedList<T> storage;
+
+        public HybridFlowProcessor()
+        {
+            storage = new DoublyLinkedList<T>();
+        }
+
+        public T Dequeue()
+        {
+            if (storage.Length == 0)
+                throw new InvalidOperationException("The storage is empty.");
+            return storage.RemoveAt(0);
+        }
+
+        public void Enqueue(T item)
+        {
+            storage.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (storage.Length == 0)
+                throw new InvalidOperationException("The storage is empty.");
+            return storage.RemoveAt(0);
+        }
+
+        public void Push(T item)
+        {
+            storage.AddAt(0, item);
+        }
+    }
+}
